5650c9f668d36ba7f19cd82225b9db7ad9ecbbde
pro <>
2018-04-15 11:23:36 -0700

    Test1

---
+++ calculator.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    if(isset($_POST['submit'])){
+        
+    }
+?>
+
+<h2>Calculator</h2>
+<form action="welcome_get.php" method="get" >
+First Number:<br>
+<input type="text" name="firstname">
+<br>
+Secon Number:<br>
+<input type="text" name="lastname">
+<input type="submit">
+</form>
+
+</body>
+</html>
