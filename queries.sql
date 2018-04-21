ccfe8ecbe7cf9965c1db14f594fd5473d80efc50
pro <>
2018-04-20 22:53:09 -0700

    1. Change all comment sign -- to # for create.sql, queries.sql, and violate.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
---Select all the movie title and year that are shown after 2000
+# Select all the movie title and year that are shown after 2000
 SELECT title,
 		year
 FROM Movie
 WHERE year > 2000; 
 
---Select the director id and their corresponding movie id;
+# Select the director id and their corresponding movie id;
 SELECT 
 	did,
 	mid
@@ -12,7 +12,7 @@ FROM Director
 INNER JOIN MovieDirector
 ON Director.id = MovieDirector.did;
 
--- Select all the movie title and their corresponding movie genre
+# Select all the movie title and their corresponding movie genre
 SELECT 
 	title,
 	genre
