f2fbebbd1b48ca71a4a9e3544c2b0c08b53ea595
pro <>
2018-04-20 22:57:00 -0700

    1. Add final version in zip 2. Fix some comment in violate.sql 3. Delete calculator.php

--- violate.sql
+++ violate.sql
@@ -1,4 +1,4 @@
 INSERT INTO Movie (title) VALUES  ("ACD");  # Doest not give value to primary key movie id
-INSERT INTO Movie (id, title) VALUES  (3, "ACD"); # Duplicate id, the primary key must be uniqe
+INSERT INTO Movie (id, title) VALUES  (3, "ACD"); # Duplicate id, the primary key must be unique
 INSERT INTO Movie (id) VALUES  ("ACD");  # Not an integer value
 INSERT INTO Director(last, dob) VALUES  ("ACD",2019);  # Director must be born before 2019
